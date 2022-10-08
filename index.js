5e0fe6110169420d5b7e57a4fcfecd23d90cf51d
akaanuzman <uzmanakan@gmail.com>
2022-10-08 04:32:18 +0300

    req.url usage

--- index.js
+++ index.js
@@ -2,10 +2,20 @@
 var http = require("http");
 
 var server = http.createServer((req, res) => {
-    res.write("<h1> Hello world ! </h1>")
+    switch (req.url) {
+        case "/home":
+            res.write("<h1>home</h1>");
+            break;
+        case "/products":
+            res.write("<h1>products</h1>");
+            break;
+        default:
+            res.write("<h1> the page is not fount </h1>");
+            break;
+    }
     res.end();
 });
 
 server.listen(5098, () => {
-    console.log("node.js servers was opened at port 5098!");
+    console.log("node.js server was opened at port 5098!");
 });
