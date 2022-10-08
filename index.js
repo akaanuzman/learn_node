0341488215f19543d661b839450c59ab467b508e
akaanuzman <uzmanakan@gmail.com>
2022-10-08 23:46:36 +0300

    front

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require("express")
 const app = express()
 const port = 5097
 
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.static("node_modules"))
 
 const products = [
-    { id: 1, name: "iPhone 11", price: 18000, image: "1.webp", isActive: false },
-    { id: 2, name: "iPhone 14", price: 40000, image: "2.webp", isActive: true },
-    { id: 3, name: "iPhone 14 Pro", price: 45000, image: "3.webp", isActive: true },
-    { id: 4, name: "Macbook M1 Air", price: 20000, image: "4.webp", isActive: true },
-    { id: 5, name: "Macbook M2 Air", price: 25000, image: "5.webp", isActive: true },
+    { id: 1, name: "iPhone 11", price: 18000, image: "1.webp", isHome: false, isActive: false },
+    { id: 2, name: "iPhone 14", price: 40000, image: "2.webp", isHome: false, isActive: true },
+    { id: 3, name: "iPhone 14 Pro", price: 45000, image: "3.webp", isHome: false, isActive: true },
+    { id: 4, name: "Macbook M1 Air", price: 20000, image: "4.webp", isHome: true, isActive: true },
+    { id: 5, name: "Macbook M2 Air", price: 25000, image: "5.webp", isHome: true, isActive: true },
 ]
 
 /// Nodejs route struct
@@ -27,7 +27,9 @@ app.get("/products", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", {
+        products: products
+    })
 });
 
 app.listen(port, () => {
