008ea86c2c5997ddaa0ea4c9145a88f5373b9946
akaanuzman <uzmanakan@gmail.com>
2022-10-08 04:56:36 +0300

    changes

--- index.js
+++ index.js
@@ -5,25 +5,28 @@ var fs = require("fs");
 var server = http.createServer((req, res) => {
     switch (req.url) {
         case "/":
-            fs.readFile("views/index.html", (err, html) => {
-                res.write(html);
-                res.end();
-            })
+            readFile("views/index.html", res);
             break;
         case "/products":
-            fs.readFile("views/products.html", (err, html) => {
-                res.write(html);
-                res.end();
-            })
+            readFile("views/products.html", res);
             break;
         default:
-            fs.readFile("views/404.html", (err, html) => {
-                res.write(html);
-                res.end();
-            })
+            readFile("views/404.html", res);
             break;
     }
 });
+
+function readFile(path, res) {
+    fs.readFile(path, (err, html) => {
+        if (err) {
+            res.write("<h1>the page was crashed! </h1>");
+            res.end();
+        } else {
+            res.write(html);
+            res.end();
+        }
+    })
+}
 
 server.listen(5097, () => {
     console.log("node.js server was opened at port 5097!");
