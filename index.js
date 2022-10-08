ad93cc92c28fa25268d9032f823790e31740a84e
akaanuzman <uzmanakan@gmail.com>
2022-10-08 22:23:45 +0300

    ejs usage

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express()
 const port = 5097
 
+const products = [
+    { id: 1, name: "iPhone 11", price: 18000, isActive: false },
+    { id: 2, name: "iPhone 14", price: 40000, isActive: true },
+    { id: 3, name: "iPhone 14 Pro", price: 45000, isActive: true },
+    { id: 4, name: "Macbook M1 Air", price: 20000, isActive: true },
+    { id: 5, name: "Macbook M2 Air", price: 25000, isActive: true },
+]
+
 /// Nodejs route struct
 app.get("/products/:id", (req, res) => {
     res.render("product_details.ejs");
 });
 
 app.get("/products", (req, res) => {
-    res.render("products.ejs");
+    res.render("products.ejs", {
+        products: products
+    });
 });
 
 app.get("/", (req, res) => {
