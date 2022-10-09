887bbe885f4c7593e4cef9159e23c2342413a623
akaanuzman <uzmanakan@gmail.com>
2022-10-10 02:28:10 +0300

    todo add was complated

--- todo-mysql/routes/route.js
+++ todo-mysql/routes/route.js
@@ -2,20 +2,41 @@ const express = require("express")
 const routes = express.Router()
 const db = require("../data/db")
 
-routes.use("/", async (req, res) => {
+routes.use(express.json())
+routes.use(express.urlencoded())
+
+routes.get("/", (req, res) => {
+    fetchData(res)
+})
+
+routes.post("/", (req, res) => {
+    const todoName = req.body.todoName
+    addTodo(todoName, res)
+    console.log(req.body)
+})
+
+async function fetchData(res) {
     try {
         const [tasks,] = await db.execute("select * from Tasks where isActive = 1")
         res.render("index", {
-            tasks: tasks
+            tasks: tasks,
         })
     } catch (error) {
         console.error(error)
     }
+}
 
-})
+async function addTodo(todoName, res) {
+    try {
+        const query = "INSERT INTO `node_db`.`Tasks` (`todoName`, `createdTime`, `isActive`) VALUES (?, NOW(), '1')"
+        await db.execute(query, [todoName])
+        fetchData(res)
+    } catch (error) {
+        console.error(error)
+    }
+}
 
-
-function getDate() {
+function getDate(tasks) {
     var currentdate = new Date();
     var dateTime = currentdate.getDay() + "/" + currentdate.getMonth()
         + "/" + currentdate.getFullYear() + " @ "
